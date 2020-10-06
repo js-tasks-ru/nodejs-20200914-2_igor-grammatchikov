38481381b1f2c7051f6ec3aa7e51a0988b69d7c2
Igor Grammatchikov <igor.grammatchikov@lamoda.ru>
2020-10-07 00:40:26 +0300

    add limitStream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,11 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.options = options;
+    this.arr = [];
   }
 
   _transform(chunk, encoding, callback) {
-
-  }
+    let error;
+    this.arr.push(chunk);
+    if (this.arr.length === this.options.limit) {
+      error = new LimitExceededError;
+    }
+    callback(error, chunk);
+  };
 }
 
 module.exports = LimitSizeStream;
