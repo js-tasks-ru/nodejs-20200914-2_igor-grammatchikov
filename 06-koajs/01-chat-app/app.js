aee7bc3eec3d33cc4c41d82b1d3d838395c52627
Igor Grammatchikov <igor.grammatchikov@lamoda.ru>
2020-10-31 15:34:51 +0300

    add chat

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,12 +8,23 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const subscriptions = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const getMessage = new Promise((resolve) => {
+    subscriptions.push(resolve);
+  });
+  ctx.body = await getMessage;
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (ctx.request.body.message) {
+    subscriptions.forEach((resolve) => {
+      resolve(ctx.request.body.message);
+      ctx.status = 200;
+    });
+  }
 });
-
 app.use(router.routes());
 
 module.exports = app;
