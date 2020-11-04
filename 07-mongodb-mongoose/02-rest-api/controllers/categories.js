dbd5a83971e6ced369a9a0ec40cbb5f7e82cc8c9
Igor Grammatchikov <igor.grammatchikov@lamoda.ru>
2020-11-04 22:29:21 +0300

    add rest api and mongo

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,17 @@
+const Category = require('../models/Category');
+
+const mapSubcategory = (subcategory) => ({
+  id: subcategory._id,
+  title: subcategory.title,
+});
+
+const mapCategory = (category) => ({
+  id: category._id,
+  title: category.title,
+  subcategories: category.subcategories.map(mapSubcategory),
+});
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find();
+  ctx.body = {categories: categories.map(mapCategory)};
 };
--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,46 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
+const mapping = (obj) => ({
+  id: obj._id,
+  title: obj.title,
+  images: obj.images,
+  category: obj.category,
+  subcategory: obj.subcategory,
+  price: obj.price,
+  description: obj.description,
+});
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  ctx.searchParams = {};
+  if (!ctx.request.query.subcategory) {
+    return next();
+  }
+  ctx.searchParams.subcategory = ctx.request.query.subcategory;
+  return next();
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const product = await Product.find(ctx.searchParams);
+  if (product) {
+    ctx.body = {products: product.map(mapping)};
+  } else {
+    ctx.body = [];
+  }
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+    ctx.status = 400;
+    return;
+  }
+
+  const product = await Product.findById(ctx.params.id);
+
+  if (product) {
+    ctx.body = {product: mapping(product)};
+  } else {
+    ctx.status = 404;
+  }
 };
 
