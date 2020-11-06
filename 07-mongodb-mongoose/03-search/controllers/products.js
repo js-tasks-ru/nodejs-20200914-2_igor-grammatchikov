0914fea828fc26e681e9cb53ad30725a2fa243f3
Igor Grammatchikov <igor.grammatchikov@lamoda.ru>
2020-11-06 23:50:34 +0300

    add search

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,20 @@
+const Product = require('../models/Product');
+
+const mapping = (obj) => ({
+  id: obj._id,
+  title: obj.title,
+  images: obj.images,
+  category: obj.category,
+  subcategory: obj.subcategory,
+  price: obj.price,
+  description: obj.description,
+});
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const product = await Product.find({$text: {$search: ctx.query.query}});
+  if (product) {
+    ctx.body = {products: product.map(mapping)};
+  } else {
+    ctx.body = [];
+  }
 };
