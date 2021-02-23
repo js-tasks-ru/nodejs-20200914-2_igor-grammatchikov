2197dd4db0761ef9f68fd56d90ea6449eea63a03
Igor Grammatchikov <igor.grammatchikov@lamoda.ru>
2021-02-23 23:40:59 +0300

    add email reg

--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -1,11 +1,44 @@
 const { v4: uuid } = require('uuid');
 const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
+const Session = require('../models/Session');
 
 module.exports.register = async (ctx, next) => {
+  const {email, displayName, password} = ctx.request.body;
+  const verificationToken = uuid();
 
+  const user = new User({
+    email,
+    displayName,
+    verificationToken,
+  });
+
+  await user.setPassword(password);
+  await user.save();
+
+  await sendMail({
+    template: 'confirmation',
+    to: email,
+    subject: 'Подтвердите почту',
+    locals: {token: verificationToken},
+  });
+
+  ctx.status = 200;
+  ctx.body = {status: 'ok'};
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const user = await User.findOne({verificationToken: ctx.request.body.verificationToken});
+
+  if (!user) {
+    ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+  }
+
+  await user.set('verificationToken', undefined);
+  await user.save();
+
+  const token = uuid();
+  await Session.create({token, user, lastVisit: new Date()});
   
+  ctx.body = { token };
 };
