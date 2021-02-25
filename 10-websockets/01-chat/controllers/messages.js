ed5f35c0c4bf8130c5655321b38dd25200a48cd9
Igor Grammatchikov <igor.grammatchikov@lamoda.ru>
2021-02-25 21:18:55 +0300

    add ws

--- 10-websockets/01-chat/controllers/messages.js
+++ 10-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,12 @@
 const Message = require('../models/Message');
+const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  const messages = await Message.find({user: ctx.user.displayName});
+
+  if (!messages) {
+    ctx.body = { messages: [] };
+  }
+
+  ctx.body = { messages: messages.map(mapMessage).slice(-20)};
 };
